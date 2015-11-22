374f720ea77e9fb5411b3984dc0a3ff68d79b092
Ulrik Nielsen <uln@ihfood.dk>
2015-11-22 14:44:16 +0100

    Implemented the function makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for calculating and caching the inverse of a matrix.
+## The input matrix has to be invertible
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+    
+    # Init variable that contains the inverted matrix
+    invA <- NULL
+    
+    # Set the input matrix. Reset any previously calculated inverse
+    set <- function(B) {
+        A <<- B
+        invA <<- NULL
+    }
+    
+    # Get the currently stored input matrix
+    get <- function()
+        A
+    
+    # Set the inverse matrix
+    setinv <- function(inv)
+        invA <<- inv
+    
+    # Get currently stored inverse matrix
+    getinv <- function()
+        invA
+    
+    # Return a list with the implemented functions
+    list(
+        set = set, get = get,
+        setinv = setinv,
+        getinv = getinv
+    )
+    
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by
+## `makeCacheMatrix` above. If the inverse has already been calculated (and the
+## matrix has not changed), then `cacheSolve` retrieve the inverse from
+## the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+    ## Return a matrix that is the inverse of 'A'
+    
+    # Read the cache - retun its value if not empty.
+    invA <- A$getinv()
+    if (!is.null(invA)) {
+        message("getting cached data")
+        return(invA)
+    }
+    
+    # Calculate and store the inverse matrix if the cache is empty
+    data <- A$get()
+    invA <- solve(data, ...)
+    A$setinv(invA)
+    
+    # Return inverse
+    invA
+    
 }
